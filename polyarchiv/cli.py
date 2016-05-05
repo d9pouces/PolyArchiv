5fa52d942635ea3c3704df5178f54a424830ed1c
mgallet <mgallet@spibol.19pouces.net>
2016-05-06 00:05:15 +0200

    better doc display

--- polyarchiv/cli.py
+++ polyarchiv/cli.py
@@ -9,8 +9,10 @@ import logging
 import logging.config
 import math
 import os
+import re
 import sys
 
+import subprocess
 from pkg_resources import iter_entry_points
 
 from polyarchiv.conf import Parameter
@@ -92,29 +94,33 @@ def main():
                               local_remote_command=show_remote_local_repository,
                               only_locals=args.only_locals, only_remotes=args.only_remotes)
     elif command == 'plugins':
+        width = 80
+        tput_cols = subprocess.check_output(['tput', 'cols']).decode().strip()
+        if re.match('^\d+$', tput_cols):
+            width = int(tput_cols)
         cprint('configuration directory: %s' % args.config, YELLOW)
         if not verbose:
             cprint('display available options for each engine with --verbose', CYAN)
 
         cprint('available local repository engines:', YELLOW)
         # noinspection PyTypeChecker
-        display_classes('polyarchiv.locals', verbose=verbose)
+        display_classes('polyarchiv.locals', verbose=verbose, width=width)
         cprint('available source engines:', YELLOW)
         # noinspection PyTypeChecker
-        display_classes('polyarchiv.sources', verbose=verbose)
+        display_classes('polyarchiv.sources', verbose=verbose, width=width)
         cprint('available remote repository engines:', YELLOW)
         # noinspection PyTypeChecker
-        display_classes('polyarchiv.remotes', verbose=verbose)
+        display_classes('polyarchiv.remotes', verbose=verbose, width=width)
     return return_code
 
 
-def display_classes(plugin_category, verbose=False):
+def display_classes(plugin_category, verbose=False, width=80):
     """display plugins of a given category"""
     for entry_point in iter_entry_points(plugin_category):
         engine_cls = entry_point.load()
         cprint('  * engine=%s' % entry_point.name, BOLD, GREEN)
         if engine_cls.__doc__:
-            cprint('    ' + engine_cls.__doc__, GREY, BOLD)
+            cprint('    ' + engine_cls.__doc__.strip(), GREY, BOLD)
 
         if verbose:
             cprint('    options:', GREEN)
@@ -122,11 +128,13 @@ def display_classes(plugin_category, verbose=False):
         for parameter in engine_cls.parameters:
             assert isinstance(parameter, Parameter)
             if parameter.help_str:
+                txt = '%s: %s' % (parameter.option_name, parameter.help_str)
                 lines = []
-                for line in parameter.help_str.splitlines():
-                    lines += [line[70 * i:70 * (i + 1)] for i in range(int(math.ceil(len(line) / 70.)))]
+                w = width - 8
+                for line in txt.splitlines():
+                    lines += [line[w * i:w * (i + 1)] for i in range(int(math.ceil(len(line) / float(w))))]
                 if verbose:
-                    cprint('      - %s: %s' % (parameter.option_name, '\n        '.join(lines)), GREEN)
+                    cprint('      - ' + ('\n        '.join(lines)), GREEN)
             else:
                 if verbose:
                     cprint('      - %s' % parameter.option_name, GREEN)
