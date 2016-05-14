644c506f3b01e97015b6fd580b2ab2cbc1d36d8f
mgallet <mgallet@spibol.19pouces.net>
2016-05-14 19:09:30 +0200

    bugfix in encoding errors

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from __future__ import unicode_literals, print_function
 import sys
 import os
 sys.path += [os.path.dirname(os.path.dirname(__file__))]
@@ -6,8 +7,7 @@ sys.path += [os.path.dirname(os.path.dirname(__file__))]
 from polyarchiv.termcolor import cprint, RED
 
 print('*** python%s ***' % sys.version_info[0])
-print('LC_ALL=%s' % os.environ.get('LC_ALL', ''))
-print('LC_CTYPE=%s' % os.environ.get('LC_CTYPE', ''))
-print('sys.stdout.encoding=%s' % sys.stdout.encoding)
-print('sys.stdout.isatty()=%s' % sys.stdout.isatty())
+print('LC_ALL=%s, LC_CTYPE=%s' % (os.environ.get('LC_ALL', ''), os.environ.get('LC_CTYPE', '')))
+print('sys.stdout.encoding=%s, sys.stdout.isatty()=%s' % (sys.stdout.encoding, sys.stdout.isatty()))
 cprint('accents:éè', RED)
+print(' ')
